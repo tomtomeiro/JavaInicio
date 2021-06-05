65d8762e81abb60539409c895af10cd9d2deb19a
Tomas <tfloresgarcia94@gmail.com>
2021-06-05 14:11:49 -0500

    Repaso de tipos de datos en el uso de Java

---
+++ TiposDatos/src/tiposdatos/TiposDatos.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdatos;
+
+/**
+ *
+ * @author Admin
+ */
+public class TiposDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*En trabajo es de crear tipos de datos primitivos*/
+        byte entero = 12;
+        int uno = 1;
+        float decim= 14.5f;
+        double decimal = 17.88;
+        char caracter = 7;
+        boolean verdadero = true;
+        boolean falso = false; 
+        System.out.println("entero"+entero+" "+uno+" "+ decimal+" "+verdadero);
+        
+    }
+    
+}
