a594576867000f0bd78ba0426ac3760011b1d817
Tomas <tfloresgarcia94@gmail.com>
2021-06-05 19:01:11 -0500

    Practicado con las variables de cadenas y Integer

---
+++ DatosNoPrimitivos/src/datosnoprimitivos/DatosNoPrimitivos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datosnoprimitivos;
+
+/**
+ *
+ * @author Admin
+ */
+public class DatosNoPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       Integer nulo = null;
+       String nombre ="Juan de la garza peréz";
+       
+        System.out.println("");
+    }
+    
+}
